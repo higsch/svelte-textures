fb754a2594cddfdedfe88842c6786892ad4e88eb
Matthias Stahl <matthias.stahl@spiegel.de>
2023-03-17 14:32:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Svelte + Textures.js
+
+Example how to use textures.js with Svelte.
+Note: Textures.js is made to work with D3 selections.
+
+## Run
+
+```
+git clone https://github.com/higsch/svelte-textures.git
+cd svelte-textures
+npm i
+npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { onMount } from 'svelte';
+    import { select } from 'd3-selection';
+    import textures from 'textures';
+
+    let svgElem;
+
+    const texture = textures
+        .lines()
+        .orientation('3/8', '7/8')
+        .stroke('darkorange');
+
+    onMount(() => {
+        select(svgElem).call(texture);
+    });
+</script>
+
+<main>
+    <svg
+        width="500"
+        height="500"
+        bind:this={svgElem}
+    >
+        <circle
+            cx="250"
+            cy="250"
+            r="200"
+            stroke="1"
+            fill={texture.url()}
+        />
+    </svg>
+</main>
+
+<style>
+
+</style>
